b241388231e6c6bf19a3d58ae4b9b5c1488fc049
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-07-06 12:12:16 +0430

    feat: Checks if number is greater than 100

--- script/script.js
+++ script/script.js
@@ -1,19 +1,24 @@
 const container = document.querySelector(".container");
 const resetButton = document.querySelector(".reset-btn");
 
+// Creates div element based on input from user
 const createDiv = (numberOfDivsToCreate) => {
   numberOfDivsToCreate = prompt("Enter the number of squares per side");
 
   const divWidth = 600 / numberOfDivsToCreate;
   const divHeight = (divWidth / 600) * 100;
 
-  for (let i = 0; i < numberOfDivsToCreate * numberOfDivsToCreate; i++) {
-    const divEl = document.createElement("div");
-    divEl.classList.add("item");
+  if (numberOfDivsToCreate > 100) {
+    alert("Please enter a number between 1 and 100");
+  } else {
+    for (let i = 0; i < numberOfDivsToCreate * numberOfDivsToCreate; i++) {
+      const divEl = document.createElement("div");
+      divEl.classList.add("item");
 
-    divEl.style.setProperty("flex", `${divHeight}%`);
-    divEl.style.setProperty("height", `${divHeight}%`);
-    container.appendChild(divEl);
+      divEl.style.setProperty("flex", `${divHeight}%`);
+      divEl.style.setProperty("height", `${divHeight}%`);
+      container.appendChild(divEl);
+    }
   }
 };
 
